76e6726cde1b3b0a790b5cd9276440c4610ae3ec
tainv2 <taingovan97@gmail.com>
2025-12-09 16:27:06 +0700

    initialize auth service

---
+++ src/test/java/com/example/spabooking/SpaBookingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spabooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaBookingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
